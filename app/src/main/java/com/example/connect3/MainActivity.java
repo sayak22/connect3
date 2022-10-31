0507cf76092915bd3e5ab6b1d5edc7769dfd0aca
Sayak Mondal <sayakmondal2001@gmail.com>
2022-10-31 15:02:20 +0530

    drop In function applied to all grids

--- app/src/main/java/com/example/connect3/MainActivity.java
+++ app/src/main/java/com/example/connect3/MainActivity.java
@@ -1,10 +1,21 @@
 package com.example.connect3;
 
+import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view){
+        ImageView counter = (ImageView) view;
+        counter.setScaleX(0f);
+        counter.setScaleY(0f);
+        counter.setImageResource(R.drawable.yellow);
+        counter.animate().scaleX(1f).scaleY(1f).setDuration(200);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
